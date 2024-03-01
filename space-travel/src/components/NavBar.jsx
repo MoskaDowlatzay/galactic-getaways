572e4ba0f8e99f48a5ea35642a33fd093896463f
MoskaDowlatzay <moskadowlatzay@hotmail.com>
2024-03-01 22:30:59 +0000

    navbar code ammended

--- space-travel/src/components/NavBar.jsx
+++ space-travel/src/components/NavBar.jsx
@@ -3,12 +3,15 @@ import { Navbar, Nav } from 'react-bootstrap';
 
 function NavBar() {
     return (
-        <Navbar bg="dark" variant="dark" expand="lg" style={{ position: 'fixed', width: '100%', zIndex: '1000' }}>           
+        <Navbar variant="dark" expand="lg" style={{ position: 'fixed', width: '100%', zIndex: '1000' }}>           
             <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto" style={{ marginLeft: '30px' }}>
+                <Nav className="d-flex justify-content-between w-100 me-auto" style={{ marginLeft: '30px' }}>
+                    <div className='d-flex flex-row'>
                     <Nav.Link href="#home" style={{ marginRight: '30px' }}>Home</Nav.Link>
                     <Nav.Link href="#explore" style={{ marginRight: '30px' }}>Explore</Nav.Link>
                     <Nav.Link href="#contact" style={{ marginRight: '30px' }}>Contact</Nav.Link>
+                    </div>
+                    <Nav.Link href="#favourites" style={{ marginRight: '30px' }}>Favourites</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
