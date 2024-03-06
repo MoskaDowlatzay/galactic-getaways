08bcabe8065ee02fbd4c093bb80f3f7e7256a75d
MoskaDowlatzay <moskadowlatzay@hotmail.com>
2024-03-06 21:29:59 +0000

    contact form created

--- space-travel/src/components/Contact.jsx
+++ space-travel/src/components/Contact.jsx
@@ -1,8 +1,113 @@
+import React from "react";
 
 export default function Contact() {
-    return (
-        <>
-            <p style={{color: "white"}}>This is the Team Contact page</p>
-        </>
-    )
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [message, setMessage] = React.useState("");
+
+  function encode(data) {
+    return Object.keys(data)
+      .map(
+        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+      )
+      .join("&");
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", name, email, message }),
+    })
+      .then(() => alert("Message sent!"))
+      .catch((error) => alert(error));
+  }
+
+  return (
+    <section id="contacts" className="d-flex justify-content-center" style={{ paddingTop: "5rem" }}>
+      <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap justify-content-center">
+        <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end relative">
+          <div className="bg-gray-900 flex-wrap py-6 rounded shadow-md">
+            <div className="lg:w-1/2 px-6">
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                ADDRESS
+              </h2>
+              <p className="mt-1 text-white">
+                galactic head office <br />
+                out of this world
+              </p>
+            </div>
+            <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                EMAIL
+              </h2>
+              <a className="text-white leading-relaxed">
+                contact@galactic-getaways.com
+                <br></br>
+                <br></br>
+              </a>
+            </div>
+          </div>
+        </div>
+        <form
+          netlify
+          name="contact"
+          onSubmit={handleSubmit}
+          className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+          <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
+            Contact Us
+            <br></br>
+          </h2>
+          <p className="leading-relaxed mb-5 text-white">
+            If you have any questions or queries fill out the form below,
+            <br></br>
+            or you can reach out to us on our socials in the footer.
+          </p>
+          <div className="relative mb-4">
+            <label htmlFor="name" className="leading-7 text-sm text-white">
+              Name&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="relative mb-4">
+            <label htmlFor="email" className="leading-7 text-sm text-white">
+              Email&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-white py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="relative mb-4">
+            <label
+              htmlFor="message"
+              className="leading-7 text-sm text-white">
+              Message&nbsp;&nbsp;&nbsp;&nbsp;
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-black py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+              onChange={(e) => setMessage(e.target.value)}
+            />
+          </div>
+          <button
+            type="submit"
+            className="text-blue bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+            Submit
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 }
