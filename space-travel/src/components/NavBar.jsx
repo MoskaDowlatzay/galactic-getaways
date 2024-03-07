854d9f8d381e29c4472a699b0313d9e011ac7a21
blaztted <bbc.silva@gmail.com>
2024-03-07 19:15:14 +0000

    now the navbar is always fixed to the top

--- space-travel/src/components/NavBar.jsx
+++ space-travel/src/components/NavBar.jsx
@@ -1,29 +1,47 @@
-import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
-import 'bootstrap-icons/font/bootstrap-icons.css';
+import React from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 function NavBar() {
-    // const navigate = useNavigate();
+  // const navigate = useNavigate();
 
-    return (
-        <Navbar variant="dark" expand="lg" style={{width: '100%', zIndex: '1000'}}>           
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="d-flex justify-content-between w-100 me-auto" style={{ marginLeft: '30px' }}>
-                    <div className='d-flex flex-row'>
-                    <Nav.Link href="/home" style={{ marginRight: '30px' }}>Home</Nav.Link>
-                    <Nav.Link href="/explore" style={{ marginRight: '30px' }}>Explore</Nav.Link>
-                    <Nav.Link href="/contact" style={{ marginRight: '30px' }}>Contact</Nav.Link>
-                    </div>
-                    <a href="/favorites" onClick={() => navigate("/favorites")}>
-                        <i className="bi bi-star" style={{ marginRight: '30px', color: 'white', fontSize: '24px' }}></i>
-                    </a>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-    );
+  return (
+    <Navbar
+      variant="dark"
+      expand="lg"
+      style={{ width: "100%", zIndex: "1000" }}
+      fixed="top"
+    >
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav
+          className="d-flex justify-content-between w-100 me-auto"
+          style={{ marginLeft: "30px" }}
+        >
+          <div className="d-flex flex-row">
+            <Nav.Link href="/home" style={{ marginRight: "30px" }}>
+              Home
+            </Nav.Link>
+            <Nav.Link href="/explore" style={{ marginRight: "30px" }}>
+              Explore
+            </Nav.Link>
+            <Nav.Link href="/contact" style={{ marginRight: "30px" }}>
+              Contact
+            </Nav.Link>
+          </div>
+          <a href="/favorites" onClick={() => navigate("/favorites")}>
+            <i
+              className="bi bi-star star"
+              style={{
+                marginRight: "30px",
+                color: "white",
+                fontSize: "24px",
+              }}
+            ></i>
+          </a>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 }
 
 export default NavBar;
-
-
-
